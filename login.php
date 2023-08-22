21645d0a6d9937374a9682562159a61060dd5958
masarayaqoob <121818449+masarayaqoob@users.noreply.github.com>
2023-08-22 17:35:57 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+
+    $con = new mysqli("localhost","root","","user_db");
+    if($con->connect_error){
+        die("failed to connect :".$con->connect_error);
+    }else {
+        $stmt= $con->prepare("select * from login where username = ?");
+        $stmt->bind_param("s",$username);
+        $stmt->execute();
+        $stmt_result= $stmt->get_result();
+        if($stmt_result->num_rows > 0){
+            $data =$stmt_result->fetch_assoc();
+            if($data['password']=== $password){
+                echo"Login Successfully";
+            }else{
+                echo"invalid username or password";
+            }
+
+        } else {
+            echo"invalid username or password"; 
+        }
+    }
+
+?>
